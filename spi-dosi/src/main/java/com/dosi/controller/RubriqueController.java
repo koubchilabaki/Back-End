b311562dcf241c0b56ac3f005e5e7bf9202ff9d7
Srir Omar <omar.srir14@gmail.com>
2020-03-22 16:17:58 +0100

    Correction rubriqueController.Java

--- spi-dosi/src/main/java/com/dosi/controller/RubriqueController.java
+++ spi-dosi/src/main/java/com/dosi/controller/RubriqueController.java
@@ -60,7 +60,7 @@ public class RubriqueController {
     	
     	if(!rubriqueService.findIfIdRubriqueExistsInRbEvalRbQst(idRubrique)) {
     		
-    		return new ResponseEntity<String>("Impossible de supprimer la rubrique",HttpStatus.BAD_REQUEST);
+    		return new ResponseEntity<String>("Impossible de supprimer la rubrique",HttpStatus.OK);
     	}else {
     		rubriqueService.deleteRubrique(idRubrique);
     		return new ResponseEntity<String>("Suppression reussite",HttpStatus.OK);
